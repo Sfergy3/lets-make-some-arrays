d83c028dbadfc0bd22cb0d26235dc6a88382fcbc
s3 <stanleyl.fergusoniii@gmail.com>
2023-02-11 02:19:05 +0000

    meeshka mooshka mickeeey mouse!

---
+++ arrayofsunshine.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# arrays 
+# Stanley L. Ferguson III
+# 9 Feb 2023
+# mkdirs and text files in thos dirs
+
+mkdir red_ray orange_ray yellow_ray green_ray
+
+ sunshine=(./red_ray/ ./orange_ray/ ./yellow_ray/ ./green_ray/)
+
+ touch "${sunshine[0]}mickey.txt"
+ touch "${sunshine[1]}donald.txt"
+ touch "${sunshine[2]}goofy.txt"
+ touch "${sunshine[3]}minnie.txt"
+
+ # main
+
+ # end
+
+ 
